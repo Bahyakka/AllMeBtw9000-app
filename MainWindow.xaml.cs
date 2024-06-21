5a307f183bb9efcaec7c957649fbac0c08c31d9d
Bahy4kka <magnoliy105@hotmail.com>
2024-06-22 00:02:02 +0300

    Release version 1.0
    Made almost perfect

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Text.RegularExpressions;
+namespace AllMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+   public partial class MainWindow : Window
+
+    {
+       
+
+        public MainWindow() => InitializeComponent();
+
+      
+        private void Button_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            string text = txtInput.Text;
+
+            string pattern = @"(>>\d+)";
+
+            string replacement = "$1(You)";
+
+            string v = Regex.Replace(text, pattern, replacement);
+            string newText = v;
+
+
+
+           
+
+
+
+            txtOutput.Text = v;
+
+        }
+
+    }
+}
